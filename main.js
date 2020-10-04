3da7f716d0b9d016504a955a8a7884ee4e3e442b
Chris Kamp <chriskamp101@gmail.com>
2020-10-04 10:44:55 +1000

    Add function to initialise board HTML

--- main.js
+++ main.js
@@ -1,19 +1,31 @@
 const Gameboard = (() => {
     const board = ["X","O","X","O","X","O","X","O","X"];
     return {board};
- })();
+})();
 
 const displayController = (() => {
-     const displayBoard = () => {
+    const board = [];
+    const initialiseBoard = () => {
+        const gridContainer = document.getElementById("grid-container");
+        for(i = 0; i< 9; i++) {
+            const square = document.createElement("div");
+            square.classList.add("boardSquare");
+            gridContainer.appendChild(square);
+            square.textContent = Gameboard.board[i]; //Move to displayBoard
+            board.push(square);
+        }
+    };
 
-     };
-     return {displayBoard};
- })
+    const displayBoard = () => {
 
-function Player() {
+    };
+    return {displayBoard, initialiseBoard};
+ })();
+
+function Player(mark) {
     let score = 0;
     return {score};
- }
+}
 
-const player1 = Player();
-const player2 = Player();
+const player1 = Player("X");
+const player2 = Player("O");
